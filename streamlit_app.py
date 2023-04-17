a1d6f1a471ddfe9f1aa0cb38597ac2c144fbf02e
Hanxiao Xiang <200750432@mail.dhu.edu.cn>
2023-04-17 19:33:21 +0800

    upload requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,21 @@ st.write('# NPV Calculator')
 # let user upload a excel
 uploaded_file = st.file_uploader("Choose a file")
 
-# get the file name
-file_name = uploaded_file.name
+if uploaded_file is not None:
+    # get the file name
+    file_name = uploaded_file.name
 
-# save the file
-with open(file_name, 'wb') as f:
-    f.write(uploaded_file.getbuffer())
+    # save the file
+    with open(file_name, 'wb') as f:
+        f.write(uploaded_file.getbuffer())
 
-# use function to calculate NPV
-npv_calculate(file_name)
+    # use function to calculate NPV
+    npv_calculate(file_name)
 
-# show the result from circular_table.xlsx
-st.write('## Result')
-# read circular_table.xlsx
-df = pd.read_excel('circular_table.xlsx', header=None)
-# show the result
-st.dataframe(df)
+    # show the result from circular_table.xlsx
+    st.write('## Result')
+    # read circular_table.xlsx
+    df = pd.read_excel('circular_table.xlsx', header=None)
+    # show the result
+    st.dataframe(df)
 
